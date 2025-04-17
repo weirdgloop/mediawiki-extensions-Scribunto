c2829ff033021d7d0c9fbcb4b11c1d4e8b6a54c2
MusikAnimal <musikanimal@gmail.com>
2025-04-17 19:30:47 -0400

    Add integration with Extension:CodeMirror
    Uses a very similar integration strategy to what CodeEditor does.
    $wgScribuntoUseCodeMirror is off by default, with CodeEditor still
    being the default integration.
    
    Bug: T373711
    Change-Id: Ifb9230de8519c90840350c645d7b16e56766a01a

--- includes/Engines/LuaCommon/LuaEngine.php
+++ includes/Engines/LuaCommon/LuaEngine.php
@@ -407,6 +407,11 @@ abstract class LuaEngine extends ScribuntoEngineBase {
 	}
 
 	/** @inheritDoc */
+	public function getCodeMirrorLanguage() {
+		return 'lua';
+	}
+
+	/** @inheritDoc */
 	public function runConsole( array $params ) {
 		// $resetFrames is a ScopedCallback, so it has a purpose even though it appears unused.
 		$resetFrames = $this->setupCurrentFrames();
